b6895929e61ccb78c0bca43abc28e2d4355d21e9
Dev Patel <114556134+DevPatel1412@users.noreply.github.com>
2024-06-27 11:03:32 +0530

    Update ModelTesting.py
    remove the code for the dotenv

--- ModelTesting.py
+++ ModelTesting.py
@@ -2,10 +2,10 @@ import os
 import streamlit as st
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from huggingface_hub import login
-from dotenv import load_dotenv
+
 
 # Load environment variables
-load_dotenv()
+
 HFT = os.environ['HF_TOKEN']
 login(HFT)
 
